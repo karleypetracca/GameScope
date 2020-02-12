26db60b2ffbe010737fbd42a0800bd28dd480a02
Dustin Myers <dustinblaine.myers@gmail.com>
2020-02-12 18:21:25 -0500

    added function to call api endpoint

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,15 @@
 "use strict";
 
-//
+const getReleaseDates = function() {
+    const apiUrl = `https://www.giantbomb.com/api/games/?api_key=6b4ac05b6e77823f1510fcb200250f6e07e11241&format=JSON&filter=expected_release_year:2020`
+    
+    
+    
+    get(apiUrl)
+    .then(function(response) {
+        console.log(response.value);
+    })
+    
+};
+
+getReleaseDates();
