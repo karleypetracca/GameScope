b9f92d2077bdc4c27c1c0595ad0d3560294d24d2
Karley Petracca <karleypetracca@gmail.com>
2020-02-17 01:47:38 -0500

    Progress made on visuals and better mobile accessibility

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,9 +1,11 @@
 
 window.onload = async function() {
-    let gameSelectId = await localStorage.getItem("gameSelectId");
+    let gameSelectId = await localStorage.getItem("gameSelectId") || "3030-57912";
     let gameObject = await queryGameSelect(gameSelectId);
-    $("#game_title").html(gameObject.name);
     $("#game_image").attr("src", gameObject.image.medium_url);
+    $("#game_title").html(gameObject.name);
+    $("#game_description").html(gameObject.deck);
+
     console.log(gameObject);
     document.getElementsByTagName("html")[0].style.visibility = "visible";
 }
